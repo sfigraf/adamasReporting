4411b700cb4a69ddc18430ec099e173cad7bbd28
Sam Graf <sfigraf@gmail.com>
2026-02-05 12:33:45 -0700

    new css specific file, navbar changes

--- app.R
+++ app.R
@@ -32,43 +32,57 @@ for (i in list.files("./modules/")) {
 ###sampleFData options for filters
 sampleFData <- tbl(CPW_AqDatAnalysis, "SampleFView")
 
-allDistinctWaters <- sampleFData %>%
-  distinct(WaterName) %>%
-  show_query() %>%
-  pull() %>%
-  sort()
+if(!exists("allDistinctWaters")){
+  allDistinctWaters <- sampleFData %>%
+    distinct(WaterName) %>%
+    show_query() %>%
+    pull() %>%
+    sort()
+}
 
-allYearssql <- c("SELECT DISTINCT year(SampleDate) FROM SampleFView")
-allYears <- dbGetQuery(CPW_AqDatAnalysis, allYearssql)
+if(!exists("allYears")){
+  allYearssql <- c("SELECT DISTINCT year(SampleDate) FROM SampleFView")
+  allYears <- dbGetQuery(CPW_AqDatAnalysis, allYearssql)
+}
 
-allBios <- sampleFData %>%
-  distinct(AreaBio) %>%
-  show_query() %>%
-  pull() %>%
-  sort()
+if(!exists("allBios")){
+  allBios <- sampleFData %>%
+    distinct(AreaBio) %>%
+    show_query() %>%
+    pull() %>%
+    sort()
+}
 
-allSPBios <- sampleFData %>%
-  distinct(SpConBio) %>%
-  show_query() %>%
-  pull() %>%
-  sort()
+if(!exists("allSPBios")){
+  allSPBios <- sampleFData %>%
+    distinct(SpConBio) %>%
+    show_query() %>%
+    pull() %>%
+    sort()
+}
 
-allStationCodes <- sampleFData %>%
-  distinct(StationCode) %>%
-  show_query() %>%
-  pull() %>%
-  sort()
+if(!exists("allStationCodes")){
+  allStationCodes <- sampleFData %>%
+    distinct(StationCode) %>%
+    show_query() %>%
+    pull() %>%
+    sort()
+}
 
-allSurveyIDs <- sampleFData %>%
-  distinct(SurveyID) %>%
-  show_query() %>%
-  pull() %>%
-  sort()
+if(!exists("allSurveyIDs")){
+  allSurveyIDs <- sampleFData %>%
+    distinct(SurveyID) %>%
+    show_query() %>%
+    pull() %>%
+    sort()
+}
 
-allLengths <- sampleFData %>%
-  distinct(Length_mm) %>%
-  show_query() %>%
-  pull() 
+if(!exists("allLengths")){
+  allLengths <- sampleFData %>%
+    distinct(Length_mm) %>%
+    show_query() %>%
+    pull() 
+}
 
 sampleFInitialFilterValues <- list(
   "allBios" = allBios,
@@ -84,19 +98,28 @@ sampleFInitialFilterValues <- list(
 ###Same thing for current summaries
 #current summary options for filters
 currentSummaryData <- tbl(CPW_AqDatAnalysis, "CurrentSummary")
-allYearsSummarizedData <- currentSummaryData %>%
-  distinct(year(SampleDate)) %>%
-  pull()
-allBiosSummarizedData <- currentSummaryData %>%
-  distinct(AreaBio) %>%
-  show_query() %>%
-  pull() %>%
-  sort()
-allDistinctWatersSummarizedData <- currentSummaryData %>%
-  distinct(WaterName) %>%
-  show_query() %>%
-  pull() %>%
-  sort()
+
+if(!exists("allYearsSummarizedData")){
+  allYearsSummarizedData <- currentSummaryData %>%
+    distinct(year(SampleDate)) %>%
+    pull()
+}
+
+if(!exists("allBiosSummarizedData")){
+  allBiosSummarizedData <- currentSummaryData %>%
+    distinct(AreaBio) %>%
+    show_query() %>%
+    pull() %>%
+    sort()
+}
+
+if(!exists("allDistinctWatersSummarizedData")){
+  allDistinctWatersSummarizedData <- currentSummaryData %>%
+    distinct(WaterName) %>%
+    show_query() %>%
+    pull() %>%
+    sort()
+}
 
 currentSummariesInitialFilterValues <- list(
   "allYearsSummarizedData" = allYearsSummarizedData, 
@@ -110,22 +133,31 @@ rsdLimits <- tbl(CPW_AqDatAnalysis, "RSDLimitsView") %>%
 ui <- fluidPage(
   navbarPage(title = div(img(src="CPWLogoLarge.png", height = "60px", style = "margin-right: 15px;"), "Adamas Reporting"), 
              #selected = c("Map"),
-             windowTitle = HTML("<title>Residual Pool Depth</title> <link rel='icon' type='image/gif/png' href='CPWLogoLarge.png'>"),
+             windowTitle = HTML("<title>Adamas Reporting</title> <link rel='icon' type='image/gif/png' href='CPWLogoLarge.png'>"),
              #this part changes the navbar options
-             tags$head(
-               tags$style(HTML('.navbar-nav > li > a, .navbar-brand {
-                            padding-top:9px !important; 
-                            padding-bottom:0 !important;
-                            height: 80px;
-                            }
-                           .navbar {min-height:25px !important;}'))
+             header = tags$head(
+               tags$link(rel = "stylesheet", type = "text/css", href = "customStyles.css")
              ),
+             
+             # tags$head(
+             #   tags$style(HTML('.navbar-nav > li > a, .navbar-brand {
+             #                padding-top:9px !important;
+             #                padding-bottom:0 !important;
+             #                height: 80px;
+             #                }
+             #               .navbar {min-height:25px !important;}'))
+             # ),
              id = "tabs", 
              theme = shinytheme("cerulean"),
-             tabPanel(tags$div("SampleF Data",style = title_style), 
-                      sampleFData_UI("sampleFData", sampleFInitialFilterValues)),  
-             tabPanel(tags$div("Summarized Data", style = title_style), 
-                      summarizedData_UI("summarizedData", currentSummariesInitialFilterValues))
+             navbarMenu("Data Source", #tags$div( ,style = title_style)
+               tabPanel("SampleFView", 
+                        sampleFData_UI("sampleFData", sampleFInitialFilterValues)),  
+               tabPanel("CurrentSummary",  
+                        summarizedData_UI("summarizedData", currentSummariesInitialFilterValues))
+             )
+             
+             #selectInput("tableSelect", label = tags$div("Data Table",style = title_style), choices = c(1,2,3)),
+             
   )    
 
 )
