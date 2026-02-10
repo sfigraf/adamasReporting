c63436634198022b57144629b6a39c9d339018a3
Sam Graf <sfigraf@gmail.com>
2026-02-10 10:12:42 -0700

    added "updating" reactive value to remove "flickers" in validate

--- modules/lengthFrequencyInputsMod.R
+++ modules/lengthFrequencyInputsMod.R
@@ -50,8 +50,15 @@ lengthFrequencyInputs_Server <- function(id) {
     id,
     function(input, output, session) {
       
+      
+      is_updating <- reactiveVal(FALSE)
       #make new shinyvalidate input validator
       iv <- InputValidator$new()
+      
+      #add condition to only apply validator rules at specifc time to avoid "required" flicker
+     # print(input$lengthFrequencyBinwidthOptions)
+      #iv$condition(~ input$lengthFrequencyBinwidthOptions == target_binwidth())
+      iv$condition(~ !is_updating())
       # add rules for numeric input
       iv$add_rule("lengthFrequencyBinwidthOptions", sv_required()) # Ensure it's not empty
       iv$add_rule("lengthFrequencyBinwidthOptions", sv_numeric())  # Ensure it's a number
@@ -61,15 +68,30 @@ lengthFrequencyInputs_Server <- function(id) {
       #enable validator
       iv$enable()
       
+      
+      
       observeEvent(input$lengthFrequency_LengthOptions, {
-        req(iv$is_valid())
+        #print(input$lengthFrequencyBinwidthOptions)
+        #req(iv$is_valid())
+        #iv$disable() 
+        is_updating(TRUE)
         #for graph rendering stuff; tells shiny not to let anything else use binwidth value until it finishes updating
         # prevents "double render/flicker when graph renders in app. Shouldn't make a dif in report rendering ui
         freezeReactiveValue(input, "lengthFrequencyBinwidthOptions")
         
         numericInputVal <- if (input$lengthFrequency_LengthOptions == "Length_inch") 1 else 10
         updateNumericInput(session, "lengthFrequencyBinwidthOptions", value = numericInputVal)
+        
+        session$onFlushed(function() {
+          later::later(function() { is_updating(FALSE) }, 0.1)
+        }, once = TRUE)
+        
       })
+      # target_binwidth <- eventReactive(input$lengthFrequency_LengthOptions, {
+      #   req(input$lengthFrequency_LengthOptions)
+      #   if (input$lengthFrequency_LengthOptions == "Length_inch") 1 else 10
+      # })
+      
       
       #inputs to return to parent module
       return(
