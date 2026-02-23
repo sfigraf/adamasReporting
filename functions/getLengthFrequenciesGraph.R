a1f1f01b3d04a81048219d275a04a0ca6e84f2f9
Sam Graf <sfigraf@gmail.com>
2026-02-23 10:48:47 -0700

    added species code to rsd hover graph

--- functions/getLengthFrequenciesGraph.R
+++ functions/getLengthFrequenciesGraph.R
@@ -1,4 +1,4 @@
-
+#lengthOptions <- "RSD"
 getLengthFrequenciesGraph <- function(data, lengthOptions, binwidth, rsdLimits){
   
   if(lengthOptions == "RSD"){
@@ -12,12 +12,12 @@ getLengthFrequenciesGraph <- function(data, lengthOptions, binwidth, rsdLimits){
       left_join(rsdLimits, by = "SpeciesCode") %>%
       mutate(RSD = replace_na(RSD, "Below Stock Size")) %>%
       mutate(hoverText = case_when(
-        RSD == "Below Stock Size" ~ paste0(CommonName, "<br> Below Stock Size: <", SLEN, "mm <br>", "Count: ", Count), 
-        RSD == "Stock" ~ paste0(CommonName, "<br> 'Stock' Range (mm): ", SLEN, " - ", QLEN, "<br>", "Count: ", Count), 
-        RSD == "Quality" ~ paste0(CommonName, "<br> 'Quality' Range (mm): ", QLEN, " - ", PLEN, "<br>", "Count: ", Count), 
-        RSD == "Preferred" ~ paste0(CommonName, "<br> 'Preferred' Range (mm): ", PLEN, " - ", MLEN, "<br>", "Count: ", Count), 
-        RSD == "Memorable" ~ paste0(CommonName, "<br> 'Memorable' Range (mm): ", MLEN, " - ", TLEN, "<br>", "Count: ", Count), 
-        RSD == "Trophy" ~ paste0(CommonName, "<br> 'Trophy' Range (mm): ", TLEN, "+", "<br>", "Count: ", Count), 
+        RSD == "Below Stock Size" ~ paste0(CommonName, " (", SpeciesCode, ")", "<br> Below Stock Size: <", SLEN, "mm <br>", "Count: ", Count), 
+        RSD == "Stock" ~ paste0(CommonName, " (", SpeciesCode, ")", "<br> 'Stock' Range (mm): ", SLEN, " - ", QLEN, "<br>", "Count: ", Count), 
+        RSD == "Quality" ~ paste0(CommonName, " (", SpeciesCode, ")", "<br> 'Quality' Range (mm): ", QLEN, " - ", PLEN, "<br>", "Count: ", Count), 
+        RSD == "Preferred" ~ paste0(CommonName, " (", SpeciesCode, ")", "<br> 'Preferred' Range (mm): ", PLEN, " - ", MLEN, "<br>", "Count: ", Count), 
+        RSD == "Memorable" ~ paste0(CommonName, " (", SpeciesCode, ")", "<br> 'Memorable' Range (mm): ", MLEN, " - ", TLEN, "<br>", "Count: ", Count), 
+        RSD == "Trophy" ~ paste0(CommonName, " (", SpeciesCode, ")", "<br> 'Trophy' Range (mm): ", TLEN, "+", "<br>", "Count: ", Count), 
         #with assigning "Below Stock Size" to the NA values, this should never come up so if it does it's worth investigating why
         TRUE ~ "No RSD Assigned"
       ), 
